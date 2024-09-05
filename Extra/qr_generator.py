18d4ee21e06bfa3ce706da1d80e7ef8dde5a24c4
Sofia Salinas <unitargetofsofi@gmail.com>
2024-09-05 10:42:02 -0500

    qr generator help

--- Extra/qr_generator.py
+++ Extra/qr_generator.py
@@ -1,10 +1,12 @@
 import qrcode
  
+rooms = [ 1714, 1814, 2315, 2414 , 2512]
 # Data to be encoded
-data = { "room_number": 2514, "building": "Foundry" }
- 
-# Encoding data using make() function
-img = qrcode.make(data)
- 
-# Saving as an image file
-img.save('2415.png')
+for room in rooms:
+    data = { "room_number": room, "building": "Foundry" }
+    
+    # Encoding data using make() function
+    img = qrcode.make(data)
+    
+    # Saving as an image file
+    img.save(f'{room}.png')
